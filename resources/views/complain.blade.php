50225469c3ed9bba3c5b2f891a31053193d1da14
xeroctation <umidakosimova.uk@gmail.com>
2022-05-07 11:26:07 +0500

    blade

--- resources/views/complain.blade.php
+++ resources/views/complain.blade.php
@@ -9,7 +9,7 @@
                     <form action="" method="post" enctype="multipart/form-data">
                         @csrf
                         <input type="hidden" name="song_filename" value="{{$song->filename}}">
-                        Write the complaint reason:
+                        Write the complaint reason please:
                         <input type="text" name="complain_text">
                         <input type="submit" value="Send Complaint">
                     </form>
